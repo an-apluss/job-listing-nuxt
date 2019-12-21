4d4be194f66e0035f80ba338bb1cb4606f5d9e13
an-apluss <anuoluwapoakinseye@gmail.com>
2019-12-21 01:48:36 +0100

    add user interface to view a specific job detail

--- pages/jobs/_id/view/index.vue
+++ pages/jobs/_id/view/index.vue
@@ -1,5 +1,72 @@
 <template>
-  <div>
-    <h1>specific job view</h1>
-  </div>
+   <div>
+    <v-container >
+      <nuxt-link to="/">
+        <v-btn class="ma-2" color="orange darken-2" dark>
+          <v-icon dark left>mdi-arrow-left</v-icon>Back
+        </v-btn>
+      </nuxt-link>
+      <v-row align="center">
+        <v-col
+          cols="12"
+        >
+          <v-card
+            class="pa-2"
+          >
+            <v-card-text class="teal white--text">
+              <h1>{{job.company}}</h1>
+            </v-card-text>
+            <v-card-text>
+              <h3>Job Title</h3>
+              <p class="mt-1">
+                {{job.title}}
+              </p>
+              <h3>Industry</h3>
+              <p class="mt-1">
+                {{job.industry}}
+              </p>
+              <h3>Job Description</h3>
+              <p class="mt-1">
+                {{job.description}}
+              </p>
+              <h3>Salary</h3>
+              <p class="mt-1">
+                #{{job.salary}}
+              </p>
+              <h3>Location</h3>
+              <p class="mt-1">
+                {{job.location}}
+              </p>
+              <h3>Job Type</h3>
+              <p class="mt-1">
+                {{job.job_type}}
+              </p>
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+   </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  head () {
+    return {
+      title: 'Jobbase | Job Details'
+    }
+  },
+
+  async fetch({store, params}) {
+    await store.dispatch('jobs/fetchSpecificJob', {id: params.id})
+  },
+
+  computed: {
+    ...mapState({
+      job: state => state.jobs.job
+    })
+  }
+}
+</script>
--- store/jobs.js
+++ store/jobs.js
@@ -2,31 +2,40 @@ import api from '~/api/index'
 
 export const state = () => ({
   list: [],
+  job: {},
 })
 
 export const mutations = {
-  index(state, data) {
-    state.list = data
+  index(state, jobs) {
+    state.list = jobs
+  },
+
+  fetchOne(state, job) {
+    state.job = job
   },
 
   reset(state) {
     state.list = []
-  }
+    state.job = {}
+  },
+
 }
 
 export const actions = {
   async fetchAllJob({commit}) {
-    // await api.job.getAllJob()
-    //   .then(response => {
-    //     commit('get_all_job', response.data.data)
-    //   })
-    //   .catch(error => {
-    //     commit('reset_job_list')
-    //   })
     try {
       const { data } = await api.job.getAllJob()
       commit('index', data.data)
       return data.data
+    } catch (error) {
+      commit('reset')
+    }
+  },
+
+  async fetchSpecificJob({commit}, params) {
+    try {
+      const { data } = await api.job.getSpecificJob(params.id)
+      commit('fetchOne', data.data)
     } catch (error) {
       commit('reset')
     }
