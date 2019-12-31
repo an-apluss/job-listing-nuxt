732e37b46cccce2ec870d83922f535b01b431612
an-apluss <anuoluwapoakinseye@gmail.com>
2019-12-31 08:53:18 +0100

    adjust navigation button

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -5,7 +5,7 @@
         <nuxt-link to="/" class="font-weight-bold black--text">JOBBASE</nuxt-link>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="teal--text text-capitalize" nuxt href="/signin" exact color="white">signin</v-btn>
+      <v-btn class="teal--text text-capitalize mr-3" nuxt href="/signin" exact color="white">signin</v-btn>
       <v-btn class="teal--text mr-10 text-capitalize" nuxt href="/register" exact color="white">Register</v-btn>
     </v-toolbar>
   </div>
