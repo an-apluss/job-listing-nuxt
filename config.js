bff3f80b07ee658f064bd72ca1d36172d7dc1be6
an-apluss <anuoluwapoakinseye@gmail.com>
2019-12-31 07:24:23 +0100

    change app base URL

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-export const baseURL = 'http://localhost:8000/api/'
-// export const baseURL = 'https://jobbase-api.herokuapp.com/api/'
+export const baseURL = 'https://jobbase-api.herokuapp.com/api/'
